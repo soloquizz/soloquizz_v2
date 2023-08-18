14852c9f86fda64d26b9e1fb0beee59e7d967138
catm2017 <cheikhmbengue146@gmail.com>
2023-08-18 22:27:31 +0000

    Finish correct display certification and publie a certification

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -3,6 +3,7 @@
 use \App\Models\Administration\Etudiant;
 use \App\Models\Administration\Certification;
 use \App\Models\Administration\Classe;
+use \App\Models\Administration\Editeur;
 
 if (!function_exists('getNbreEtudiants')) {
     function getNbreEtudiants()
@@ -24,6 +25,17 @@ if (!function_exists('getNbreClasses')) {
         return Classe::count();
     }
 }
+
+if (!function_exists('getEtudiantCertifications')) {
+    function getEtudiantCertifications(Editeur $editeur)
+    {
+        return $editeur->certifications->filter(function ($certification){
+            return $certification->statut and $certification->niveau?->id == auth()->user()?->etudiant()?->classe()?->niveau?->id;
+        });
+    }
+}
+
+
 
 
 
